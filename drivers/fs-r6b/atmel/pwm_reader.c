f46e04a18c53ded6e27e957434ca5447c9011043
jluceno <luceno@usc.edu>
2019-04-01 17:15:45 -0700

    Caluclated percentages, changed output pins.

--- drivers/fs-r6b/atmel/pwm_reader.c
+++ drivers/fs-r6b/atmel/pwm_reader.c
@@ -10,26 +10,28 @@
 #define CYCLES_0_THROTTLE 922
 #define CYCLES_100_THROTTLE 1843
 #define NUM_CHAN 4
-#define SET_PWM_PC_MASK (1<<PCINT0) | (1<<PCINT1) | (1<<PCINT2) | (1<<PCINT3)
-#define PWM_DDB_MASK (1<<DDB0) | (1<<DDB1) | (1<<DDB2) | (1<<DDB3)
-#define PWM_PINB0 (PINB & (1<<PINB0))
-#define PWM_PINB1 (PINB & (1<<PINB1))
-#define PWM_PINB2 (PINB & (1<<PINB2))
-#define PWM_PINB3 (PINB & (1<<PINB3))
-#define DEBUG_ON PORTC |= (1<<PORTC0)
-#define DEBUG_OFF PORTC &= ~(1<<PORTC0)
+#define SET_PWM_PC_MASK (1<<PCINT8) | (1<<PCINT9) | (1<<PCINT10) | (1<<PCINT11)
+#define PWM_DDC_MASK (1<<DDC0) | (1<<DDC1) | (1<<DDC2) | (1<<DDC3)
+#define PWM_PINC0 (PINC & (1<<PINC0))
+#define PWM_PINC1 (PINC & (1<<PINC1))
+#define PWM_PINC2 (PINC & (1<<PINC2))
+#define PWM_PINC3 (PINC & (1<<PINC3))
+#define DEBUG_ON PORTB |= (1<<PORTB0)
+#define DEBUG_OFF PORTB &= ~(1<<PORTB0)
 #define TIMR_PRESC (1<<CS11)
 
 unsigned int PWM_time_i[NUM_CHAN] = {0, 0, 0, 0};
 unsigned int PWM_time_d[NUM_CHAN] = {0, 0, 0, 0};
+unsigned int PWM_max_d[NUM_CHAN] = {0, 0, 0, 0};
+unsigned int PWM_min_d[NUM_CHAN] = {UINT32_MAX, UINT32_MAX, UINT32_MAX, UINT32_MAX};
 unsigned int PWM_percent[NUM_CHAN] = {0, 0, 0, 0};
 unsigned int PWM_stat_i[NUM_CHAN] = {0, 0, 0, 0};
 
-ISR(PCINT0_vect)
+ISR(PCINT1_vect)
 {
   int curr_time = TCNT1;
   int i;
-  char PWM_stat[NUM_CHAN] = {PWM_PINB0, PWM_PINB1, PWM_PINB2, PWM_PINB3};
+  char PWM_stat[NUM_CHAN] = {PWM_PINC0, PWM_PINC1, PWM_PINC2, PWM_PINC3};
 
   // Check the pins
 
@@ -38,14 +40,12 @@ ISR(PCINT0_vect)
     // Rising Edge
     if (PWM_stat_i[i] == 0 && PWM_stat[i])
     {
-      DEBUG_ON;
       PWM_stat_i[i] = 1;
       PWM_time_i[i] = TCNT1;
     }
     // Falling Edge
     else if (PWM_stat_i[i] == 1 && !PWM_stat[i])
     {
-      DEBUG_OFF;
       PWM_stat_i[i] = 0;
 
       // Overflow prevention
@@ -57,6 +57,21 @@ ISR(PCINT0_vect)
       {
         PWM_time_d[i] = curr_time - PWM_time_i[i];
       }
+
+      if(PWM_time_d[i] < PWM_min_d[i])
+        PWM_min_d[i] = PWM_time_d[i];
+      
+      if(PWM_time_d[i] > PWM_max_d[i])
+        PWM_max_d[i] = PWM_time_d[i];
+
+      // Percentage calculation
+      if(PWM_max_d[i] == PWM_min_d[i])
+        PWM_percent[i] = 0;
+      else
+      {
+        long val = ((long)PWM_time_d[i] -  (long)PWM_min_d[i])*100;
+        PWM_percent[i] = val/(PWM_max_d[i] - PWM_min_d[i]);
+      }
     }
   }
 }
@@ -65,18 +80,18 @@ int main(void)
 {
 
 // Setup debug LED
-DDRC |= (1<<DDC0);
+DDRB |= (1<<DDB0);
 
 // Setup serial debug
 serial_init (47);
 stdout = &uart_output;
 
 // Setup PWM input pins
-DDRB &= ~(PWM_DDB_MASK);
+DDRC &= ~(PWM_DDC_MASK);
 
 // Setup pins for pin change interrupts
-PCMSK0 |= SET_PWM_PC_MASK;
-PCICR |= (1<<PCIE0);
+PCMSK1 |= SET_PWM_PC_MASK;
+PCICR |= (1<<PCIE1);
 
 // Setup timer
 TCCR1B |= TIMR_PRESC;
@@ -87,8 +102,7 @@ sei();
 
 while (1)
 {
-  printf("test\n");
-  printf("%d \n", PWM_time_d[0]);
+  printf("%u, %u, %u, %u \n", PWM_percent[0], PWM_percent[1], PWM_percent[2], PWM_percent[3]);
   _delay_ms(500);
 }
 
