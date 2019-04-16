866a9ef7b3fe2171f0c5ae44228a20f01cd8ab81
jluceno <luceno@usc.edu>
2019-04-16 14:26:43 -0700

    Manually calibrated ATmega

--- drivers/fs-r6b/atmel/pwm_reader.c
+++ drivers/fs-r6b/atmel/pwm_reader.c
@@ -30,15 +30,16 @@
 unsigned int PWM_stat_i1[2] = {0, 0};
 unsigned int PWM_time_i1[2] = {0, 0};
 unsigned int PWM_time_d1[2] = {0, 0};
-unsigned int PWM_max_d1[2] = {0, 0};
-unsigned int PWM_min_d1[2] = {UINT32_MAX, UINT32_MAX};
+unsigned int PWM_max_d1[2] = {1659, 1712};
+unsigned int PWM_min_d1[2] = {769, 819};
 unsigned char PWM_percent1[2] = {0, 0};
 unsigned int PWM_stat_i2[2] = {0, 0};
 unsigned int PWM_time_i2[2] = {0, 0};
 unsigned int PWM_time_d2[2] = {0, 0};
-unsigned int PWM_max_d2[2] = {0, 0};
-unsigned int PWM_min_d2[2] = {UINT32_MAX, UINT32_MAX};
+unsigned int PWM_max_d2[2] = {1805, 1714};
+unsigned int PWM_min_d2[2] = {909, 816};
 unsigned char PWM_percent2[2] = {0, 0};
+unsigned char output;
 uint8_t message_reg = 0;
 
 
@@ -68,11 +69,11 @@ void update_values(int curr_time, char PWM_stat, unsigned int* PWM_stat_i,
     }
 
     // Calibration
-    if(*PWM_time_d < *PWM_min_d)
-      *PWM_min_d = *PWM_time_d;
+    //if(*PWM_time_d < *PWM_min_d)
+    //  *PWM_min_d = *PWM_time_d;
     
-    if(*PWM_time_d > *PWM_max_d)
-      *PWM_max_d = *PWM_time_d;
+    //if(*PWM_time_d > *PWM_max_d)
+    //  *PWM_max_d = *PWM_time_d;
 
     // Percentage calculation
     if(*PWM_max_d == *PWM_min_d)
@@ -83,57 +84,61 @@ void update_values(int curr_time, char PWM_stat, unsigned int* PWM_stat_i,
     {
       long val = ((long)(*PWM_time_d) -  (long)(*PWM_min_d))*100;
       *res = (unsigned char)(val/(*PWM_max_d - *PWM_min_d));
+      if(*res > 100)
+        *res = 100;
     }
   }
 }
 
 ISR(PCINT1_vect)
 {
-  int i;
   char PWM_stat1[2] = {PWM_PINC0, PWM_PINC1};
 
-  for(i = 0; i < 2; i++)
-  {
-    update_values(TCNT1, PWM_stat1[i], &PWM_stat_i1[i], &PWM_time_i1[i],
-     &PWM_time_d1[i], &PWM_max_d1[i], &PWM_min_d1[i], &PWM_percent1[i]);
-  }
+  // CH3
+  update_values(TCNT1, PWM_stat1[0], &PWM_stat_i1[0], &PWM_time_i1[0],
+    &PWM_time_d1[0], &PWM_max_d1[0], &PWM_min_d1[0], &PWM_percent1[0]);
+  // CH4
+  update_values(TCNT1, PWM_stat1[1], &PWM_stat_i1[1], &PWM_time_i1[1],
+    &PWM_time_d1[1], &PWM_max_d1[1], &PWM_min_d1[1], &PWM_percent1[1]);
 }
 
 ISR(PCINT0_vect)
 {
-  int i;
   char PWM_stat2[2] = {PWM_PINB0, PWM_PINB1};
 
-  for(i = 0; i < 2; i++)
-  {
-    update_values(TCNT1, PWM_stat2[i], &PWM_stat_i2[i], &PWM_time_i2[i],
-     &PWM_time_d2[i], &PWM_max_d2[i], &PWM_min_d2[i], &PWM_percent2[i]);
-  }
+  // CH1
+  update_values(TCNT1, PWM_stat2[0], &PWM_stat_i2[0], &PWM_time_i2[0],
+    &PWM_time_d2[0], &PWM_max_d2[0], &PWM_min_d2[0], &PWM_percent2[0]);
+  // CH2
+  update_values(TCNT1, PWM_stat2[1], &PWM_stat_i2[1], &PWM_time_i2[1],
+    &PWM_time_d2[1], &PWM_max_d2[1], &PWM_min_d2[1], &PWM_percent2[1]);
 }
 
 void reg_handler(uint8_t message)
 {
   message_reg = message;
+  if(message_reg == CH1)
+    output = PWM_percent2[0];
+  else if(message_reg == CH2)
+    output = PWM_percent2[1];
+  else if(message_reg == CH3)
+    output = PWM_percent1[0];
+  else if(message_reg == CH4)
+    output = PWM_percent1[1];
+  else if(message_reg == VOLTAGE)
+    output = 255;
   return;
 }
 
 void message_handler()
 {
-  if(message_reg == CH1)
-    TWDR = PWM_percent2[0];
-  else if(message_reg == CH2)
-    TWDR = PWM_percent2[1];
-  else if(message_reg == CH3)
-    TWDR = PWM_percent1[0];
-  else if(message_reg == CH4)
-    TWDR = PWM_percent1[1];
-  else if(message_reg == VOLTAGE)
-    TWDR = 255;
+  TWDR = output;
 }
 
 int main(void)
 {
 // Setup serial debug
+// Remove this on final build
 serial_init (47);
 stdout = &uart_output;
 
@@ -159,8 +164,13 @@ sei();
 
 while (1)
 {
-  printf("%u, %u, %u, %u \n", PWM_percent1[0], PWM_percent1[1], PWM_percent2[0], PWM_percent2[1]);
-  _delay_ms(500);
+  //print mins and maxes
+  //printf("%u, %u, %u, %u \n", PWM_max_d2[0], PWM_max_d2[1], PWM_max_d1[0], PWM_max_d1[1]);
+  //printf("%u, %u, %u, %u \n", PWM_min_d2[0], PWM_min_d2[1], PWM_min_d1[0], PWM_min_d1[1]);
+  
+  //print current percentages
+  //printf("%u, %u, %u, %u \n", PWM_percent2[0], PWM_percent2[1], PWM_percent1[0], PWM_percent1[1]);
+  //_delay_ms(500);
 }
 
 // Never reached
